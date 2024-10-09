544068ab5dbefb7e68b65be5aa9759c47a119538
Brendan Morrissey <bmorrissey@ggc.edu>
2024-10-09 13:53:57 -0400

    initial

---
+++ src/components/pages/Resume.js
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import resumeOne from '../assets/images/Resume2024.pdf';
+
+export default function Resume() {
+  return (
+    <div>
+      <iframe src={resumeOne} width="100%" height="600px"></iframe>
+    </div>
+  );
+}
